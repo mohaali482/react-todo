c9fe486826d38fbf3160a5df11b82c00a4c9ab32
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 22:01:23 +0300

    Add more styling

--- src/components/Todos/styled/ListItem.js
+++ src/components/Todos/styled/ListItem.js
@@ -6,4 +6,16 @@ min-height: 50px;
 display: flex;
 align-items: center;
 justify-content: space-between;
+border: 1px solid #D60E15;
+margin-top: 20px;
+border-radius: .5rem;
+padding: 5px;
+fill: #D60E15;
+&:nth-child(even){
+    background-color: #D60E15;
+    color: black;
+    & path{
+        fill: black;
+    }
+}
 `
