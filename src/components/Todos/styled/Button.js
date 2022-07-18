cda5db6234ca9a3ddb1b7f0f63516f547eeb1939
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 16:28:50 +0300

    Add styled button

---
+++ src/components/Todos/styled/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+width: 20%;
+height: 36px;
+font-size: large;
+background-color: #D60E15;
+color: white;
+border: none;
+position: absolute;
+right: 5%;
+border-radius: 25rem;
+cursor: pointer;
+`
