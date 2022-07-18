7b8963b89d497c00b7e807c6eee15d6ba80714d4
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 22:02:27 +0300

    Add access to reducer to child

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -5,16 +5,17 @@ import initialState from './state';
 import Todos from './Todos';
 import { Container } from './styled/Container';
 import { TitleHeader } from './styled/TitleHeader';
-
+import { NavButton } from './styled/NavButton';
+import { CenteredDiv } from './styled/CenteredDiv';
 
 
 const Todo = () => {
-  const [todos, dispatch] = useReducer(reducer, initialState);
+  const [{todos}, dispatch] = useReducer(reducer, initialState);
   return (
     <Container>
         <TitleHeader>Todos</TitleHeader>
         <FormInput dispatch={dispatch}/>
-        <Todos todos={todos} />
+        <Todos todos={todos} dispatch={dispatch}/>
     </Container>
   )
 }
