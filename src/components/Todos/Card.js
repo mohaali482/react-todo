cbfc0cd534154a2a88f0215ef93ff02bff9a8058
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 22:00:55 +0300

    Add check uncheck and delete functions in the card

--- src/components/Todos/Card.js
+++ src/components/Todos/Card.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import { CenteredDiv } from './styled/CenteredDiv'
 import { ListItem } from './styled/ListItem'
+import { ReactComponent as CancelLogo } from '../../assests/icons/cancel.svg'
+import { ReactComponent as CheckLogo } from '../../assests/icons/check.svg'
+import { ReactComponent as DeleteLogo } from '../../assests/icons/delete.svg'
 
-const Card = () => {
-    
+const Card = ({todo, handleDelete, handleCheck, handleUnCheck}) => {
   return (
     <ListItem>
-        <p>
-          <span>1.</span>
-          Todo.........
+        <p style={{'fontWeight': 'bold', 'paddingLeft': '20px'}}>
+          <span>{todo.id} </span>
+          {todo.name}
         </p>
         <CenteredDiv>
-          <button>check</button>
-          <button>edit</button>
-          <button>delete</button>
+          {
+          todo.isChecked ? 
+          <button onClick={()=> handleUnCheck(todo.id)} style={{'border':'none', 'background':'none'}}>
+            {<CancelLogo />}
+          </button> :
+          <button onClick={()=> handleCheck(todo.id)} style={{'border':'none', 'background':'none'}}>
+            {<CheckLogo />}
+          </button>
+          }
+          <button onClick={()=> handleDelete(todo.id)} style={{'border':'none', 'background':'none'}}>
+            {<DeleteLogo />}
+          </button>
         </CenteredDiv>
     </ListItem>
   )
