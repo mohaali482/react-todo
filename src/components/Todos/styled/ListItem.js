2f974ea2eeb6bad59f2ea610e4365ce1988a9775
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 16:37:40 +0300

    Add list item styled div

---
+++ src/components/Todos/styled/ListItem.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ListItem = styled.div`
+min-width: 83%;
+min-height: 50px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
