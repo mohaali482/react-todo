0db5616d2a32249f4d89ebffd5c6b827b184513b
Mohammed Ali <mohaali482@gmail.com>
2022-07-18 22:00:16 +0300

    Add check uncheck and delete functions

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -2,10 +2,47 @@ import React from 'react'
 import Card from './Card'
 import { CenteredDiv } from './styled/CenteredDiv'
 
-const Todos = ({todos}) => {
+const Todos = ({todos, dispatch}) => {
+  const handleDelete = (id)=>{
+    dispatch({
+      type:"DELETE",
+      payload:{
+        id
+      }
+    })
+  }
+
+  const handleCheck = (id)=>{
+    dispatch({
+      type:"COMPLETE",
+      payload:{
+        id
+      }
+    })
+  }
+
+  const handleUnCheck = (id)=>{
+    dispatch({
+      type:"UNCOMPLETE",
+      payload:{
+        id
+      }
+    })
+  }
+
   return (
     <CenteredDiv>
-        <Card />
+      <div style={{'width': '90%'}}>
+        {todos.map(todo=>{
+            if (todo !== undefined){
+              return (
+                        <Card key={todo.id} todo={todo}
+                        handleDelete={handleDelete} handleCheck={handleCheck}
+                        handleUnCheck={handleUnCheck}/>
+              )
+            }
+        })}
+      </div>
     </CenteredDiv>
   )
 }
